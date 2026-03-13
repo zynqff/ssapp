3a8f500e7e9d334521fd2724646c1c13b9e4109b
zynqff <zynqff@gmail.com>
2026-03-13 18:17:32 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
-    // Убираем версии здесь — Flutter подставит их сам
     id("com.android.application") apply false
     id("com.android.library") apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    // Если у тебя в проекте есть этот плагин, добавь и его (часто нужен для Flutter)
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") apply false // УБРАЛИ ВЕРСИЮ "1.0.0"
 }
