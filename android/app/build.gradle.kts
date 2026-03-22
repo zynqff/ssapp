df874a6fd3f97d977904e578fa049f70b8fb7303
zynqff <zynqff@gmail.com>
2026-03-22 20:04:31 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.sscollective.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 2
+        versionName = 1.0.2
     }
 
     // --- ПОДПИСЬ ---
