e3c5d4320c654ce753c05d5a9400ad96016fb08c
zynqff <zynqff@gmail.com>
2026-03-23 07:43:51 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") apply false
     id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false  // добавить
     id("dev.flutter.flutter-gradle-plugin") apply false // УБРАЛИ ВЕРСИЮ "1.0.0"
 }
