aa532f84c8c266abbeab3c35a520f74f847571cf
zynqff <zynqff@gmail.com>
2026-03-29 01:20:39 +0700

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import '../providers/auth_provider.dart';
 import '../providers/theme_provider.dart';
 
